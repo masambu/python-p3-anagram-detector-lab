611cae27834c465b05c63417cf6ef075bc593f71
Kelvin Masambu <kevohmasambu@gmail.com>
2024-11-08 10:53:45 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word 
+        self.sorted_word = sorted(word) 
+
+    def match(self, word_list):
+        return [x for x in word_list if sorted(x) == self.sorted_word] or [] 
+        
+
+bobby = Anagram('lien')
+print(bobby.match(['enlists', 'google', 'inlets', 'banana']))
